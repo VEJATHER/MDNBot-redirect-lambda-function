455d0cefa0ffaaf4fc0628362bd5e15d353ab8ce
Mihai <mihaiveronica@ymail.com>
2017-03-20 20:14:20 +0200

    pulled credentials in separate env values

--- bot.js
+++ bot.js
@@ -1,30 +1,35 @@
 var promise = require('request-promise');
 var ApiBuilder = require("claudia-api-builder");
 var api = new ApiBuilder();
+var credentials = require('./environment');
 
 api.get("/hello/{name}", function(request) {
     var name = request.pathParams.name;
     return "Hello World - meet " + name;
 });
 
-api.get("/slack", function(request) {
+api.get("/slack", function(request,response) {
             var token = request.queryString.code;
-            var client_id = "149347005137.156085577376";
-            var client_secret = "c0960085e4c495bcd4f98aa33eae9063";
-            var data = {
-                client_id: client_id,
-                client_secret: client_secret,
-                code: token
-            };
             if (!token) { // access denied
                 return 'https://vejather.github.io/mdn-bot-landing-page';
             }
-            var AUTH_URL = 'https://slack.com/api/oauth.access?client_id=' + client_id + '&client_secret=' + client_secret + '&code=' + token;
+            var AUTH_URL = 'https://slack.com/api/oauth.access?scope=commands+team%3Aread&client_id=' + credentials.client_id + '&client_secret=' + credentials.client_secret + '&code=' + token;
 
             return promise(setOptions(AUTH_URL)).then(function(data) {
                var TEAM_URL = 'https://slack.com/api/team.info?token=' + data.access_token;
-                return "MDNBot has been added to your team";
+                //return "MDNBot has been added to your team";
+                return promise(setOptions('https://slack.com/api/team.info?token='+data.access_token)).then(function (data) { 
+                      //return "team info"+JSON.stringify(data);
+                        if(data.error == 'missing_scope') {
+                             return 'MDNBot has been added to your team!';
+                           } else {
+                             var team = data.team.domain;
+                             return 'http://' +team+ '.slack.com';
+                           }
 
+
+
+                    });
             });
         });
 
